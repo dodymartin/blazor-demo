1d5beed2ddef6a5e5380a76640bdc5980a53b59f
Cody Rush <cody.rush@outlook.com>
2021-12-30 19:05:53 -0600

    Changed to repository pattern

---
+++ RushAg/Server/Data/DataRepository.cs
@@ -0,0 +1,43 @@
+﻿using RushAg.Shared;
+
+namespace RushAg.Server.Data;
+
+public class DataRepository : IDataRepository
+{
+    //TODO Need an interface for the dbContext
+    private readonly RushAgDbContext _db;
+
+    public DataRepository(RushAgDbContext db)
+    {
+        _db = db;
+    }
+    public TodoItem Add(TodoItem item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TodoItem item)
+    {
+        _db.TodoItems.Remove(item);
+    }
+
+    public IEnumerable<TodoItem> GetAll()
+    {
+        return _db.TodoItems;
+    }
+
+    public TodoItem? GetById(long id)
+    {
+        return _db.TodoItems.FirstOrDefault(x => x.Id == id);
+    }
+
+    public TodoItem Update(TodoItem item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Save()
+    {
+        return (_db.SaveChanges() >= 0);
+    }
+}
---
+++ RushAg/Server/Data/RushAgDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RushAg.Shared;
+
+namespace RushAg.Server.Data
+{
+    public class RushAgDbContext : DbContext
+    {
+        public RushAgDbContext(DbContextOptions<RushAgDbContext> options)
+            : base(options)
+        { }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+
+    
+}
