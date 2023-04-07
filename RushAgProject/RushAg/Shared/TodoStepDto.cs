2baca03dd8c0d7e7773a16d59ae260aaccac6ae6
Cody Rush <cody.rush@outlook.com>
2023-04-07 15:31:13 -0500

    Styling changes and cleanup.

--- RushAgProject/RushAg/Shared/TodoStepDto.cs
+++ RushAgProject/RushAg/Shared/TodoStepDto.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RushAg.Shared;
 
-namespace RushAg.Shared
+public class TodoStepDto
 {
-    public class TodoStepDto
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsCompleted { get; set; }
-        public DateTime? CompleteDate { get; set; }
-        public DateTime CreateDate { get; set; }
-    }
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime? CompleteDate { get; set; }
+    public DateTime CreateDate { get; set; }
+}
 
-    public class CreateTodoStepDto
-    {
-        public long TodoItemId { get; set; }
-        public string Name { get; set; }
-    }
+public class CreateTodoStepDto
+{
+    public long TodoItemId { get; set; }
+    public string Name { get; set; }
 }
