8d0c8160c0a0395e0a1145e81b6efb8190476f5f
dodymartin <cody.rush@outlook.com>
2023-06-01 21:49:54 -0500

    Started working on adding Polly.  Got a retry policy added, but need to create my named client.

--- BlazorDemo/Client/Program.cs
+++ BlazorDemo/Client/Program.cs
@@ -2,12 +2,23 @@ using BlazorDemo.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorDemo.Client.Services;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Need to create a typed HttpClient class.
+builder.Services.AddHttpClient("api",
+    client =>
+    {
+        client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    })
+    .AddTransientHttpErrorPolicy(policyBuilder =>
+        policyBuilder.WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5)));
+
 builder.Services.AddScoped<TodoService>();
 
 await builder.Build().RunAsync();
