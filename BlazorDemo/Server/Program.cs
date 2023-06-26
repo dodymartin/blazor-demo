0f31483a2b60bc0cc48eb62ec403270ac7f51307
dodymartin <cody.rush@outlook.com>
2023-06-25 20:35:29 -0500

    Changes to the Program.cs to match up to what was in the Pluralsight course.

--- BlazorDemo/Server/Program.cs
+++ BlazorDemo/Server/Program.cs
@@ -8,8 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("RushAgDb");
 builder.Services.AddDbContext<BlazorDemoDbContext>(options =>
@@ -23,25 +24,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
+//app.UseBlazorFrameworkFiles();
+//app.UseStaticFiles();
 
 app.UseRouting();
 
 app.AddTodoApis();
 
-app.MapRazorPages();
+//app.MapRazorPages();
 //app.MapControllers();
-app.MapFallbackToFile("index.html");
+//app.MapFallbackToFile("index.html");
 
 app.Run();
